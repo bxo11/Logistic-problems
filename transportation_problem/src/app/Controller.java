dc74efc63e4302842f99b9f13620019bc78c0583
KrzysiekJa <kjmj77@gmail.com>
2021-03-21 13:57:51 +0100

    Added elements in Controller

--- transportation_problem/src/app/Controller.java
+++ transportation_problem/src/app/Controller.java
@@ -1,4 +1,23 @@
 package app;
 
 public class Controller {
+    private TextField podaz1;
+    @FXML
+    private TextField podaz2;
+    @FXML
+    private TextField popyt1;
+    @FXML
+    private TextField popyt2;
+    @FXML
+    private Text result;
+    @FXML
+    private TextField y11;
+    @FXML
+    private TextField y12;
+    @FXML
+    private TextField y21;
+    @FXML
+    private TextField y22;
+
+
 }
